2a986a3e411e1a041d55ffd7787d80237002de8d
Sergey Pankratov <zergey.gad@gmail.com>
2021-12-20 02:48:38 +0300

    changed CreateGamePage

---
+++ server/src/main/resources/static/js/utils.js
@@ -0,0 +1,5 @@
+// scroll to the top of the document
+function scrollUp() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
