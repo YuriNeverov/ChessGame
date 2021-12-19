c0990709f38eec60a7ed6bc7606960d0beb06da8
Mr-Ravil <ravil.galiev.2000@mail.ru>
2021-12-19 22:28:58 +0300

    added game create validation

--- server/src/main/java/chessgame/web/server/controller/CreateGamePage.java
+++ server/src/main/java/chessgame/web/server/controller/CreateGamePage.java
@@ -2,20 +2,31 @@ package chessgame.web.server.controller;
 
 import chessgame.web.server.domain.Game;
 import chessgame.web.server.form.GameCredentials;
-import chessgame.web.server.form.UserCredentials;
+import chessgame.web.server.form.validator.GameCredentialsCreateValidator;
 import chessgame.web.server.service.GameService;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 public class CreateGamePage extends Page {
     private final GameService gameService;
+    private final GameCredentialsCreateValidator gameCredentialsCreateValidator;
 
-    public CreateGamePage(GameService gameService) {
+    public CreateGamePage(GameService gameService, GameCredentialsCreateValidator gameCredentialsCreateValidator) {
         this.gameService = gameService;
+        this.gameCredentialsCreateValidator = gameCredentialsCreateValidator;
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.addValidators(gameCredentialsCreateValidator);
     }
 
     @GetMapping("/create_game")
@@ -25,10 +36,14 @@ public class CreateGamePage extends Page {
     }
 
     @PostMapping("/create_game")
-    public String createGame(@ModelAttribute("gameForm") GameCredentials gameFrom,
+    public String createGame(@Valid @ModelAttribute("gameForm") GameCredentials gameForm,
+                             BindingResult bindingResult,
                              HttpSession httpSession) {
+        if (bindingResult.hasErrors()) {
+            return "CreateGamePage";
+        }
 
-        Game game = gameService.create(gameFrom, getUser(httpSession).getLogin());
+        Game game = gameService.create(gameForm, getUser(httpSession).getLogin());
         setGame(httpSession, game);
         putMessage(httpSession, "Congrats, you have created the game!");
 
