c30e03a45df50a8fc7c782e7d56892707dc5e09a
Mr-Ravil <ravil.galiev.2000@mail.ru>
2021-12-20 04:16:31 +0300

    added FEN to render board

--- server/src/main/java/chessgame/web/server/controller/Page.java
+++ server/src/main/java/chessgame/web/server/controller/Page.java
@@ -1,12 +1,20 @@
 package chessgame.web.server.controller;
 
 import chessgame.web.server.domain.Game;
+import chessgame.web.server.domain.Move;
 import chessgame.web.server.domain.User;
 import chessgame.web.server.service.UserService;
+import engine.Board;
+import engine.Cell;
+import engine.ChessPiece;
+import engine.Color;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpSession;
+
+import static engine.ChessPiece.EMPTY;
+import static engine.ChessPiece.KING;
 
 public class Page {
     private static final String USER_ID_SESSION_KEY = "userId";
@@ -32,8 +40,52 @@ public class Page {
         httpSession.removeAttribute(USER_ID_SESSION_KEY);
     }
 
-    void setGame(HttpSession httpSession, Game game) {
+    engine.Move moveToEngineMove(Move move) {
+        return new engine.Move(
+                new Cell(move.getXFrom(), move.getYFrom()),
+                new Cell(move.getXTo(), move.getYTo())
+        );
+    }
 
+    String getFEN(Board board) {
+        StringBuilder FEN = new StringBuilder();
+        for (int y = 7; y >= 0; y--) {
+            int spaceLength = 0;
+
+            for (int x = 0; x < 8; x++) {
+                Cell cell = new Cell(x, y);
+                ChessPiece chessPiece = board.getPiece(cell);
+                if (chessPiece == EMPTY) {
+                    spaceLength++;
+                } else {
+                    if (spaceLength != 0) {
+                        FEN.append(spaceLength);
+                        spaceLength = 0;
+                    }
+                    char partFen = switch (chessPiece) {
+                        case PAWN -> 'p';
+                        case ROOK -> 'r';
+                        case KNIGHT -> 'n';
+                        case BISHOP -> 'b';
+                        case QUEEN -> 'q';
+                        case KING -> 'k';
+                        default -> throw new IllegalStateException("Unexpected value: " + chessPiece);
+                    };
+                    if (board.getPieceColor(cell) == Color.WHITE) {
+                        partFen = Character.toUpperCase(partFen);
+                    }
+                    FEN.append(partFen);
+                }
+            }
+
+            if (spaceLength != 0) {
+                FEN.append(spaceLength);
+            }
+
+            FEN.append('/');
+        }
+        FEN.deleteCharAt(FEN.length() - 1);
+        return FEN.toString();
     }
 
     void putMessage(HttpSession httpSession, String message) {
