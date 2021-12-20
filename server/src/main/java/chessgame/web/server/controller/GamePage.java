f7873b84fa9cbaeb2fbaef8cc9bd9fbc865a70f4
Mr-Ravil <ravil.galiev.2000@mail.ru>
2021-12-20 05:38:27 +0300

    added moves

--- server/src/main/java/chessgame/web/server/controller/GamePage.java
+++ server/src/main/java/chessgame/web/server/controller/GamePage.java
@@ -2,6 +2,7 @@ package chessgame.web.server.controller;
 
 import chessgame.web.server.domain.Game;
 import chessgame.web.server.domain.Move;
+import chessgame.web.server.form.MoveCredentials;
 import chessgame.web.server.service.GameService;
 import chessgame.web.server.service.MoveService;
 import engine.Board;
@@ -10,6 +11,9 @@ import engine.MatrixBoard;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -42,8 +46,36 @@ public class GamePage extends Page {
 
         model.addAttribute("isFlip", game.getLoginBlack().equals(getUser(httpSession).getLogin()));
         model.addAttribute("FEN", getFEN(board));
-        model.addAttribute("currentPlayerColor", board.getCurrentPlayerColor());
 
         return "GamePage/" + id;
+    }
+
+    @MessageMapping("/sendMove")
+    @SendTo("/topic/public")
+    public String sendMessage(@Payload MoveCredentials moveCredentials, HttpSession httpSession) {
+        Game game;
+        try {
+            game = gameService.findById(moveCredentials.getGameId());
+        }
+        catch (NumberFormatException e) {
+            putMessage(httpSession, "This game does not exist");
+            return "redirect:/";
+        }
+
+        List<Move> moves = moveService.findAllMovesByGameId(game.getId());
+        Board board = new MatrixBoard();
+        for (Move move : moves) {
+            board.makeMove(moveToEngineMove(move));
+        }
+        long moveNumber = moves.size();
+        Move nextMove = moveService.ConvertToMove(moveCredentials, moveNumber);
+
+        if (board.makeMove(moveToEngineMove(nextMove))) {
+            moveService.makeMove(nextMove);
+        } else {
+            putMessage(httpSession, "This move is wrong");
+        }
+
+        return getFEN(board);
     }
 }
--- server/src/main/java/chessgame/web/server/service/MoveService.java
+++ server/src/main/java/chessgame/web/server/service/MoveService.java
@@ -1,6 +1,9 @@
 package chessgame.web.server.service;
 
 import chessgame.web.server.domain.Move;
+import chessgame.web.server.domain.User;
+import chessgame.web.server.form.MoveCredentials;
+import chessgame.web.server.form.UserCredentials;
 import chessgame.web.server.reprository.MoveRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,21 @@ public class MoveService {
 
     public MoveService(MoveRepository moveRepository) {
         this.moveRepository = moveRepository;
+    }
+
+    public Move ConvertToMove(MoveCredentials moveCredentials, long moveNumber) {
+        Move move = new Move();
+        move.setMoveNumber(moveNumber);
+        move.setGameId(moveCredentials.getGameId());
+        move.setXFrom(moveCredentials.getXFrom());
+        move.setYFrom(moveCredentials.getYFrom());
+        move.setXTo(moveCredentials.getXTo());
+        move.setYTo(moveCredentials.getYTo());
+        return move;
+    }
+
+    public void makeMove(Move move) {
+        moveRepository.save(move);
     }
 
     public List<Move> findAllMovesByGameId(long gameId) {
